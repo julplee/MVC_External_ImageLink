f2e0adfc660d132cbdb945e0ede13e3fd3a199ec
Julien Plée <julplee@gmail.com>
2014-07-02 11:29:53 +0200

    Solution creation

---
+++ ExternalImageLink/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExternalImageLink.MvcApplication" Language="C#" %>
---
+++ ExternalImageLink/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExternalImageLink.Startup))]
+namespace ExternalImageLink
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
